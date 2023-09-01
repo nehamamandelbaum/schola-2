0bd22a451874e259810ee6889d6312225e53e6a9
NehamaMandelbaum <nehama1836@gmail.com>
2023-09-01 14:24:05 -0300

    index and show students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,14 +2,31 @@
 
 class StudentsController < ApplicationController
   before_action :set_student, only: %i[show edit update destroy]
+  before_action :set_year, only: %i[show index]
 
   def index
-    @students = Student.all
-
-    Student.select('students.id, students.name AS student_name, enrollments.code, courses.name AS course_name, courses.year').left_joins(enrollments: :course).where('courses.year' => 2016)
+    @students = Course.select('enrollments.code, students.name as student_name, students.id as student_id, courses.name as course_name, courses.year')
+                      .joins("inner join enrollments on courses.id = enrollments.course_id and courses.year = #{@year}")
+                      .joins('right join students on students.id = enrollments.student_id')
   end
 
-  def show; end
+  def show
+    @code, @course_name = Enrollment.select('enrollments.code', 'courses.name')
+                                    .joins(:course)
+                                    .where("courses.year = #{@year} and enrollments.student_id = #{params[:id]}")
+                                    .pick(
+                                      :code, :name
+                                    )
+
+    @grades = Grade.select('subjects.name as subject_name, grades.enrollment_code, avg(grades.value) as value, grades.id, students.name as student_name')
+                   .joins(enrollment: :student)
+                   .joins(enrollment: :course)
+                   .joins(exam: :subject)
+                   .where("enrollments.student_id = #{params[:id]} and courses.year = #{@year}")
+                   .group('subjects.name, grades.enrollment_code')
+
+    flash.now[:notice] = 'No grades for this year!' if @grades.empty?
+  end
 
   def new
     @student = Student.new
@@ -44,6 +61,10 @@ class StudentsController < ApplicationController
 
   def set_student
     @student = Student.find(params[:id])
+  end
+
+  def set_year
+    @year = params[:year] || Date.current.year
   end
 
   def student_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resource :dashboard, only: :show
 
   resources :students
+
+  get '/students/:year', to: 'students#index'
+  get '/students/:id/:year', to: 'students#show'
+
   resources :enrollments, param: :code
   resources :grades
   resources :teachers
